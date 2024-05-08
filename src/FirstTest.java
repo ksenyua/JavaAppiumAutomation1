e855436faa3e38aae4372309e96de786099a61d1
ksenyua <denisovaksenya@mail.ru>
2024-05-08 13:58:33 +0300

    3_2

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -41,16 +41,20 @@ public class FirstTest {
     @Test
     public void firstTest()
     {
-        waitForElementAndClick(
-                By.xpath("//*[contains(@text,'Skip')]"),
-                "Cannot find skip button",
-                5
-                );
-
+        click_button_skip();
         assertElementHasText(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Search Wikipedia",
                 "Cannot find expected text"
+        );
+    }
+
+    private void click_button_skip()
+    {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Skip')]"),
+                "Cannot find skip button",
+                5
         );
     }
 
@@ -79,6 +83,20 @@ public class FirstTest {
             );
     }
 
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds)
+    {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
     private void assertElementHasText(By by, String expected_text, String error_message) {
         WebElement element = driver.findElement(by);
         Assert.assertEquals(
@@ -86,6 +104,46 @@ public class FirstTest {
                 expected_text,
                 element.getAttribute("text")
         );
+    }
+
+    private int getCountElement(By by, String error_message,long timeoutInSeconds) {
+        int count = driver.findElements(by).size();
+        return count;
+    }
+
+    @Test
+    public void testCheckCancelSearch(){
+        click_button_skip();
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        Assert.assertTrue("Find results less than 2", getCountElement(
+                By.id("org.wikipedia:id/page_list_item_title"),
+                "elements not found",
+                5) >= 2);
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot find search field",
+                5
+        );
+
+        waitForElementNotPresent(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "X is still present of the page",
+                5
+        );
+
     }
 }
 
